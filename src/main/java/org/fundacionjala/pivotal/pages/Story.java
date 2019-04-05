b85807f55c5f55d2b73c6ab282601e1da7b9267d
rrch.cr8 <rrch.cr8@gmail.com>
2019-04-05 17:40:17 -0400

    new locators added

--- src/main/java/org/fundacionjala/pivotal/pages/Story.java
+++ src/main/java/org/fundacionjala/pivotal/pages/Story.java
@@ -1,29 +1,66 @@
 package org.fundacionjala.pivotal.pages;
+
 import org.fundacionjala.core.ui.AbstractPage;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.PageFactoryFinder;
-import org.springframework.stereotype.Component;
+import org.openqa.selenium.support.How;
 
 /**
- *  carlangas es el autor de esta
+ * @rrch is the author
  */
 public class Story extends AbstractPage {
-    //*[@id="main"]/div[1]/div/div[2]/div[2]/div[1]/section/ol/li[1]/header/div/div/div[2]/a
-    
-    @FindBy(name = "story[name]")
-    private WebElement storyName;
 
-    @FindBy(id = "story_submit_cancel_c205")
-    private WebElement cancelButton;
+    @FindBy(id = "panel_backlog_2324243")
+    private WebElement addStoryButton;
 
     @FindBy(id = "story_close_c205")
     private WebElement saveButton;
 
-    @FindBy(id="panel_backlog_2324243")
-    private WebElement addStoryButton;
+    @FindBy(id = "story_submit_cancel_c205")
+    private WebElement cancelButton;
 
+    // css selector   #view577 > section > section.model_details > form > section > fieldset > div > div.AutosizeTextarea__container___31scfkZp > textarea
+    //xpach   *[@id="view219"]/section/section[1]/form/section/fieldset/div/div[1]/textarea
+
+    @FindBy(name = "story[name]")
+    private WebElement storyName;
+
+    //@FindBy(xpath = ".//*[@id=\"view219\"]")
+    //private WebElement storyName;
+
+    //@FindBy(how = How.XPATH, using = ".//*[@id=\"view219\"]")
+    //private WebElement storyName2;
+
+
+    /**
+     * another locators for non required fields
+     */
+    @FindBy(id = "story_copy_id_value_c205")
+    private WebElement idStoryField;
+
+    //*[@id="story_type_dropdown_c517"]/span
+    @FindBy(id = "story_type_dropdown_c517")
+    private WebElement feature;
+
+    @FindBy(id = "story_estimate_dropdown_c517")
+    private WebElement points;
+
+    @FindBy(id = "story_requested_by_id_dropdown_c517")
+    private WebElement requested;
+
+    @FindBy(how = How.XPATH, using = ".//*[@id=\"add_owner_c517\"]")
+    private WebElement addStoryOwner;
+
+    @FindBy(xpath = ".//*[@id=\"blocker-edit-new\"]")
+    private WebElement blockersTextField;
+
+    @FindBy(xpath = ".//*[@id=\"view531\"]/section/section[2]/div/div/div/div[2]/div/button[1]")
+    private WebElement addblockersButton;
+
+
+    /**
+     * Basic method with the minimum requerid for create a story.
+     */
 
     public void clickAddButton() {
         action.click(addStoryButton);
@@ -41,7 +78,6 @@ public class Story extends AbstractPage {
         clickAddButton();
         setStoryNameTextField(name);
         clickSaveButton();
-
     }
 
 
