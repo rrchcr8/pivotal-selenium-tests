6c089c283f2586e1d3dc7e5a8c91b06d24a19832
88mary256 <mary.jlm.2006@gmail.com>
2019-04-08 16:57:12 -0400

    Updated header page

--- src/main/java/org/fundacionjala/pivotal/pages/Header.java
+++ src/main/java/org/fundacionjala/pivotal/pages/Header.java
@@ -1,6 +1,7 @@
 package org.fundacionjala.pivotal.pages;
 
 import org.fundacionjala.core.ui.AbstractPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.springframework.stereotype.Component;
@@ -18,13 +19,11 @@ public class Header extends AbstractPage {
     @FindBy(className = "Dropdown__button _2Oy9G__NotificationsBell__button")
     WebElement notifications;
 
-    //@FindBy(className="zWDds__Button TtSTu__Button--header Dropdown__button")
-    // List<WebElement> buttons;
-    /* profile
-       div[data-aid="ProfileDropdown"] .zWDds__Button*/
-    /* whats new
-    div[data-aid="ProductUpdatesDropdown__indicator--newHeader"] .zWDds__Button
-     */
+    @FindBy(css="div[data-aid='ProfileDropdown'] .zWDds__Button")
+    WebElement profile;
+
+    @FindBy(css="div[data-aid='ProductUpdatesDropdown__indicator--newHeader'] .zWDds__Button")
+    WebElement whatsNew;
 
     public Dashboard goToDashBoard() {
         action.click(goDashboard);
@@ -33,5 +32,24 @@ public class Header extends AbstractPage {
 
     public void openProjectMenu() {
         action.click(projecMenu);
+    }
+
+    public void openProfile() {
+        action.click(profile);
+    }
+
+    public void openWhatsNew() {
+        action.click(whatsNew);
+    }
+
+    public void openHelp() {
+        List<WebElement> buttons = driver.findElements(By.cssSelector(
+                ".zWDds__Button.TtSTu__Button--header.Dropdown__button"));
+        for (WebElement element : buttons) {
+            if(element != profile && element != whatsNew) {
+                element.click();
+                return;
+            }
+        }
     }
 }
