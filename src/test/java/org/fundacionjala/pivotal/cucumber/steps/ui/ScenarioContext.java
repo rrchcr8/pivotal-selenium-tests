1a11478edf0c67a3534dd4dfb5d8985167efb00c
Marines Lopez Soliz <mary.jlm.2006@gmail.com>
2019-04-10 07:05:27 -0400

    Added missing javadoc

--- src/test/java/org/fundacionjala/pivotal/cucumber/steps/ui/ScenarioContext.java
+++ src/test/java/org/fundacionjala/pivotal/cucumber/steps/ui/ScenarioContext.java
@@ -3,28 +3,53 @@ package org.fundacionjala.pivotal.cucumber.steps.ui;
 import java.util.HashMap;
 import java.util.Map;
 
+/** This class represent scenario context. **/
 public class ScenarioContext {
 
     private final Map<String, Object> scenarioContext;
 
     private static final ScenarioContext context = new ScenarioContext();
 
+    /** Default constructor. **/
     private ScenarioContext() {
         this.scenarioContext = new HashMap<>();
     }
 
+    /**
+     * Get context instance.
+     *
+     * @return ScenarioContext.
+     */
     public static ScenarioContext getInstance() {
         return context;
     }
 
+    /**
+     * Set an element in context.
+     *
+     * @param key   string key
+     * @param value object value
+     */
     public void setContext(final String key, final Object value) {
         this.scenarioContext.put(key.toString(), value);
     }
 
+    /**
+     * This method get context value for key.
+     *
+     * @param key string
+     * @return object value.
+     */
     public Object getContext(final String key) {
         return this.scenarioContext.get(key.toString());
     }
 
+    /**
+     * This method check if a key is in the context.
+     *
+     * @param key string
+     * @return boolean
+     **/
     public Boolean isContains(final String key) {
         return this.scenarioContext.containsKey(key);
     }
