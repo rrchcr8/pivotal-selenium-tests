28a3325e5d0cfcd949be6f7ec9e49e6a57f35f84
Rrch <rrch.cr8@gmail.com>
2019-04-09 20:27:37 -0400

    Story page

---
+++ src/test/java/org/fundacionjala/pivotal/cucumber/steps/ui/StorySteps.java
@@ -0,0 +1,9 @@
+package org.fundacionjala.pivotal.cucumber.steps.ui;
+
+import cucumber.api.java.en.Given;
+
+public class StorySteps {
+    @Given("a project called {string}")
+    public void aProjectCalledAwt(int arg0) {
+    }
+}
