007ae85e242690a6211b642f107609c63450b25a
rrch.cr8 <rrch.cr8@gmail.com>
2019-04-12 12:40:35 -0400

    travis fixes

--- src/test/java/org/fundacionjala/pivotal/cucumber/steps/ui/StorySteps.java
+++ src/test/java/org/fundacionjala/pivotal/cucumber/steps/ui/StorySteps.java
@@ -6,6 +6,9 @@ import org.fundacionjala.pivotal.pages.Project;
 import org.fundacionjala.pivotal.pages.Story;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * this is an story steps.
+ */
 public class StorySteps {
     @Autowired
     private Project project;
@@ -13,9 +16,12 @@ public class StorySteps {
     private Dashboard dashboard;
     @Autowired
     private Story story;
-
+    /**
+     * Given step.
+     * @param arg0 is the name of the project.
+     */
     @Given("a project called {string}")
-    public void aProjectCalled(String arg0) {
+    public void aProjectCalled(final String arg0) {
     dashboard.goToProject(arg0);
     story.clickAddButton();
     }
