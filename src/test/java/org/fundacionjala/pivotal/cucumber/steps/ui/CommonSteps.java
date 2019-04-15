37ca6bdc8fe5ae8edf871db23a2bec545548d32c
Rrch <rrch.cr8@gmail.com>
2019-04-15 01:29:54 -0400

    common steps created

--- src/test/java/org/fundacionjala/pivotal/cucumber/steps/ui/CommonSteps.java
+++ src/test/java/org/fundacionjala/pivotal/cucumber/steps/ui/CommonSteps.java
@@ -1,12 +1,20 @@
 package org.fundacionjala.pivotal.cucumber.steps.ui;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import io.restassured.path.json.JsonPath;
+import org.fundacionjala.core.api.RequestManager;
 import org.fundacionjala.core.ui.driver.DriverManager;
 import org.fundacionjala.pivotal.pages.Project;
+import org.fundacionjala.util.ScenarioContext;
+import org.fundacionjala.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.fundacionjala.core.Environment;
 import org.fundacionjala.pivotal.pages.Login;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Common steps.
@@ -30,4 +38,6 @@ public class CommonSteps {
         DriverManager.getInstance().getDriver().get(Environment.getInstance().getValue("url.base"));
         login.loginAs(Environment.getInstance().getValue(userNameKey), Environment.getInstance().getValue(passwordKey));
     }
+
+
 }
