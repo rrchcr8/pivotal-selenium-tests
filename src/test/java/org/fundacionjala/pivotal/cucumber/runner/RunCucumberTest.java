f1ce0ff75716f33ad5a32d04d29af4824baaeb84
88mary256 <mary.jlm.2006@gmail.com>
2019-04-16 17:02:37 -0400

    Updated properties to add login url and updated base url. Split task feature in 2 features, added missing navigation steps.

--- src/test/java/org/fundacionjala/pivotal/cucumber/runner/RunCucumberTest.java
+++ src/test/java/org/fundacionjala/pivotal/cucumber/runner/RunCucumberTest.java
@@ -3,10 +3,9 @@ package org.fundacionjala.pivotal.cucumber.runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.fundacionjala.core.Environment;
+import org.fundacionjala.core.ui.driver.DriverManager;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
-import org.fundacionjala.core.ui.driver.DriverManager;
 
 /**
  * Class which runs all features.
@@ -18,18 +17,14 @@ import org.fundacionjala.core.ui.driver.DriverManager;
 )
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 
-    /**
-     * this method execute before the tests.
-     */
+    /** This method execute before the tests. */
     @BeforeTest
     public void open() {
         // Implement.
-        DriverManager.getInstance().getDriver().get(Environment.getInstance().getValue("url.base"));
+        DriverManager.getInstance().getDriver().get(Environment.getInstance().getValue("url.login"));
     }
 
-    /**
-     * this method close the browser after the features finish.
-     */
+    /** This method close the browser after the features finish. */
     @AfterTest
     public void close() {
         DriverManager.getInstance().getDriver().quit();
---
+++ src/test/resources/features/task_with_created_project.feature
@@ -0,0 +1,13 @@
+@wip
+Feature: Task mangement
+
+  Background:
+    Given logs in with user "owner1"
+    And Project with one story
+    And Go to Dashboard
+    And Go to default project
+    And Open a story
+
+  Scenario: Create a task
+    When Create a task with text "task1"
+    Then Verify that task was created
