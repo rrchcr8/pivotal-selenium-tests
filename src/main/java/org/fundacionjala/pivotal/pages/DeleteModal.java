04ad4e8c6182b71d650f2f7bf1187a1841f49e15
Rrch <rrch.cr8@gmail.com>
2019-04-20 00:29:28 -0400

    delete modal page added

--- src/main/java/org/fundacionjala/pivotal/pages/DeleteModal.java
+++ src/main/java/org/fundacionjala/pivotal/pages/DeleteModal.java
@@ -1,4 +1,27 @@
 package org.fundacionjala.pivotal.pages;
 
-public class DeleteModal {
+import org.fundacionjala.core.ui.AbstractPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
+
+/**
+ * this is a modal that is used to confirm a story delete.
+ */
+@Component
+public class DeleteModal extends AbstractPage {
+    /**
+     * This is the delete confirm story button.
+     */
+    @FindBy(css = "button[data-aid='DeleteButton']")
+    private WebElement deleteStoryConfirmButton;
+
+    /**
+     * Click delete button.
+     **/
+    public void clickConfirmDeleteButton() {
+        this.action.click(this.deleteStoryConfirmButton);
+    }
+
+
 }
