71b9de60f1e2b0ed51c5c72a735609aacbfd45b5
88mary256 <mary.jlm.2006@gmail.com>
2019-04-23 19:35:06 -0400

    Removed implict group import by explicits

--- src/main/java/org/fundacionjala/core/ui/WebDriverAction.java
+++ src/main/java/org/fundacionjala/core/ui/WebDriverAction.java
@@ -2,7 +2,11 @@ package org.fundacionjala.core.ui;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
