8ee91d67ec588f22abc1ff61e19152e344966ccd
rrch.cr8 <rrch.cr8@gmail.com>
2019-04-24 11:25:21 -0400

    create Account POM

--- src/main/java/org/fundacionjala/pivotal/pages/CreateAccount.java
+++ src/main/java/org/fundacionjala/pivotal/pages/CreateAccount.java
@@ -1,9 +1,29 @@
 package org.fundacionjala.pivotal.pages;
 
 import org.fundacionjala.core.ui.AbstractPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
 
 /**
- * This is page for create accounts.
+ * This is page for create accounts inside Project page options.
  */
+@Component
 public class CreateAccount extends AbstractPage {
+    /**
+     * this is a text fiel for the input of a new account name.
+     */
+    @FindBy(css = "input[class='tc-account-creator__name']")
+    private WebElement newAccountNameTextField;
+
+    /**
+     * This method is used for set the text field.
+     *
+     * @param accountNameTextField is the new of the account.
+     */
+    public void setNewAccountNameTextField(final String accountNameTextField) {
+        this.action.setValue(this.newAccountNameTextField,
+                accountNameTextField);
+
+    }
 }
