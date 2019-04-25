3ddb9fd6e68ea84789a959ef537554f580c2110a
88mary256 <mary.jlm.2006@gmail.com>
2019-04-25 11:54:36 -0400

    Added all missing elements at menu page object model (#28)

--- src/main/java/org/fundacionjala/pivotal/pages/HeaderMenu.java
+++ src/main/java/org/fundacionjala/pivotal/pages/HeaderMenu.java
@@ -4,6 +4,7 @@ import org.fundacionjala.core.ui.AbstractPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,11 +16,19 @@ public class HeaderMenu extends AbstractPage {
     @FindBy(css = "a[href='/projects']")
     private WebElement showAllProjectsWorkSpaces;
 
+    @FindBy(css = "a[href='/dashboard'][class='tc_projects_menu_footer']")
+    private WebElement goToDashboard;
+
+    @FindBy(css = "a[data-aid='CreateProject']")
+    private WebElement createProject;
+
+    @FindBy(xpath = "//a[text()='Create Workspace']")
+    private WebElement createWorkspace;
+
     /** This method click on showAllProject/showAllWorkspaces. **/
     public void showAllProjectsWorkSpaces() {
         this.action.click(this.showAllProjectsWorkSpaces);
     }
-
 
     /**
      * To check if project is listed on header menu.
@@ -32,5 +41,60 @@ public class HeaderMenu extends AbstractPage {
                 "//span[@class='raw_project_name' and contains(text(),'%s')]";
         return this.action.isExistingSelector(
                 By.xpath(String.format(xpath, name)));
+    }
+
+    /**
+     * To check if project is listed on header menu.
+     *
+     * @param name Workspace name.
+     * @return Boolean true if it is seeing on contextual menu
+     */
+    public boolean isWorksapceListedOnMenu(final String name) {
+        final String xpath =
+                "//span[@class='raw_workspace_name' and contains(text(),'%s')]";
+        return this.action.isExistingSelector(
+                By.xpath(String.format(xpath, name)));
+    }
+
+    /**
+     * This method click on dashboard option menu.
+     *
+     * @return Dashboard page
+     */
+    public Dashboard goToDashboard() {
+        this.action.click(this.goToDashboard);
+        this.wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//span[text()='Projects']")));
+        return new Dashboard();
+    }
+
+    /** This method click on create Project option menu. **/
+    public void createProject() {
+        this.action.click(this.createProject);
+    }
+
+    /** This method click on create workspace option menu. **/
+    public void createWorkspace() {
+        this.action.click(this.createWorkspace);
+    }
+
+    /**
+     * This method open a workspace.
+     *
+     * @param name workspace name.
+     */
+    public void openWorkspace(final String name) {
+        this.action.click(By.xpath("//span[text()='".concat(name)
+                .concat("' and @class='raw_workspace_name']/parent::a")));
+    }
+
+    /**
+     * This method open a project.
+     *
+     * @param name project name.
+     */
+    public void openProject(final String name) {
+        this.action.click(By.xpath("//span[text()='".concat(name)
+                .concat("' and @class='raw_project_name']/parent::a")));
     }
 }
