a637702c999c84c91f879780dce392712233b119
rrch.cr8 <rrch.cr8@gmail.com>
2019-04-25 17:57:56 -0400

    2nd commit

--- src/main/java/org/fundacionjala/pivotal/pages/ProjectSettings.java
+++ src/main/java/org/fundacionjala/pivotal/pages/ProjectSettings.java
@@ -1,4 +1,11 @@
 package org.fundacionjala.pivotal.pages;
 
-public class ProjectSettings {
+import org.fundacionjala.core.ui.AbstractPage;
+import org.springframework.stereotype.Component;
+
+/**
+ * This is project setting POM.
+ */
+@Component
+public class ProjectSettings extends AbstractPage {
 }
